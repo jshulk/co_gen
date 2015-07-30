7ddb2712b30bf295c94fe54e96b5e6d72f6b94db
vjunloc <vijaytyagi.vt@gmail.com>
2015-07-30 23:23:56 +0530

    fetching the data with co + promise

---
+++ index.js
@@ -0,0 +1,29 @@
+var request = require('superagent'),
+	Q = require('q'),
+	co = require('co');
+
+var url = "https://hacker-news.firebaseio.com/v0/topstories.json";
+
+
+function getTopStoriesPromise(){
+	var deferred = Q.defer();
+	request(url)
+	.end(function(err, response){
+		if( err ){
+			deferred.reject(err);
+		} else {
+			deferred.resolve(response);
+		}
+	});
+
+	return deferred.promise;
+}
+
+co(function *(){
+	var result = yield getTopStoriesPromise();
+	console.log("result");
+	console.log(result.body);
+})
+.catch(function(){
+	console.log("failed to fetch top stories");
+});
